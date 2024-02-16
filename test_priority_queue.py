df6b068bef8c4bd634061730d6dce056958c5e62
estelleyay <129639226+estelleyay@users.noreply.github.com>
2024-02-16 08:47:34 -0500

    Add files via upload

---
+++ test_priority_queue.py
@@ -0,0 +1,24 @@
+"""Assignment 1 - Tests for class PriorityQueue  (Task 3a)
+
+This code is provided solely for the personal and private use of
+students taking the CSC148 course at the University of Toronto.
+Copying for purposes other than this use is expressly prohibited.
+All forms of distribution of this code, whether as given or with
+any changes, are expressly prohibited.
+
+All of the files in this directory are
+Copyright (c) Jonathan Calver, Diane Horton, Sophia Huynh, Joonho Kim and
+Jacqueline Smith.
+
+Module Description:
+This module will contain tests for class PriorityQueue.
+"""
+from container import PriorityQueue
+
+# TODO: Put your pytest test functions for class PriorityQueue here
+
+
+if __name__ == '__main__':
+    import pytest
+
+    pytest.main(['test_priority_queue.py'])
